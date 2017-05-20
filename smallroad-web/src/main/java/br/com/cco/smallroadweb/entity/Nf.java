efdfb5079e14bb7640787c1a1a5c10412378fa98
Luiz Nunes <luizhgnunes@ymail.com>
2017-05-20 10:33:11 -0300

    Teste de commit.

--- smallroad-web/src/main/java/br/com/cco/smallroadweb/entity/Nf.java
+++ smallroad-web/src/main/java/br/com/cco/smallroadweb/entity/Nf.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
+// Teste de commit luizhgnunes
 @Entity
 @Table(name="nf")
 public class Nf {
